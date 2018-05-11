e5f0076063d73e4f5a26c06672efa33a85a04b97
arlindo santos <arlindo.santos@tmx.com>
2018-05-11 17:19:10 -0400

    update

--- terraform-live/service1/state-backend.tf
+++ terraform-live/service1/state-backend.tf
@@ -1,13 +1,8 @@
-provider "aws" {
-  region = "us-east-1"
-  profile = "ansiblepersonal"
-}
-
 terraform {
   backend "s3" {
     encrypt = true
     bucket = "terraform-up-and-running-state.arlindo.ca"
-    key = "app1/stage/terraform.tfstate"
+    key = "awsaccount1/app1/us-east-1/stage/terraform.tfstate"
     region = "us-east-1"
     profile = "ansiblepersonal"
     dynamodb_table = "terraform-locks"
