1c4ea8624d5bd38bff53ea1102e3c38d669caa17
arlindo santos <arlindo.santos@tmx.com>
2018-05-11 17:22:08 -0400

    update

--- ansible/scripts/destroy_service1.sh
+++ ansible/scripts/destroy_service1.sh
@@ -1,1 +1,1 @@
-ansible-playbook -i inventory/stageaws/host.ini -e "ans_destroy=true" playbooks/deploy_infra_service1.yml
+ansible-playbook -i inventory/awsaccount1/us-east-1/stage/host.ini -e "ans_destroy=true" playbooks/deploy_infra_service1.yml
